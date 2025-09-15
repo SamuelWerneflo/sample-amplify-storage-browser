d22120190b5182908035e7e46543b86444eca2c5
SamuelWerneflo <160221396+SamuelWerneflo@users.noreply.github.com>
2025-09-15 17:45:14 +0200

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -34,7 +34,7 @@ backend.addOutput({
         //@ts-expect-error amplify backend type issue https://github.com/aws-amplify/amplify-backend/issues/2569
         paths: {
           "*": {
-            groupsdl: ["get", "list", "write", "delete"],
+            groupsmueldl: ["get", "list", "write", "delete"],
             groupsmuel: ["get", "list", "write", "delete"],
           },
         },
@@ -100,4 +100,4 @@ const dlPolicy = new Policy(backend.stack, "customBucketDlPolicy", {
 
 // Add the policies to the muel user role
 backend.auth.resources.groups["muel"].role.attachInlinePolicy(muelPolicy);
-backend.auth.resources.groups["dl"].role.attachInlinePolicy(dlPolicy);
+backend.auth.resources.groups["mueldl"].role.attachInlinePolicy(dlPolicy);
