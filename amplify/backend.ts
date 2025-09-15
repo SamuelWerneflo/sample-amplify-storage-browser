3de39dbf1e779123b9f67678852e841b723a50e7
SamuelWerneflo <160221396+SamuelWerneflo@users.noreply.github.com>
2025-09-15 11:35:20 +0200

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,5 +1,6 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
+import { storage } from "./auth/resource";
 import { Policy, PolicyStatement, Effect } from "aws-cdk-lib/aws-iam";
 
 /**
