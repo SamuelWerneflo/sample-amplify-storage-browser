efa3f4f1153f8bfc9197b5357e2cbd63bcb9a06e
SamuelWerneflo <160221396+SamuelWerneflo@users.noreply.github.com>
2025-09-15 12:37:19 +0200

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,10 +1,5 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const muelBucket = defineStorage({
-  name: 'samueeel',
-  access: (allow) => ({
-    'test/*': [
-      allow.groups(['muel']).to(['read', 'write', 'delete'])
-    ]
-  })
+  name: 'samueeel'
 });
