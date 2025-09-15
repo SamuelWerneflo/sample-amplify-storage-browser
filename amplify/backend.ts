11b90836c7af92d08aee1655c8387f3b36929dfa
SamuelWerneflo <160221396+SamuelWerneflo@users.noreply.github.com>
2025-09-15 16:53:05 +0200

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -16,6 +16,17 @@ backend.addOutput({
   storage: {
     buckets: [
       {
+        name: "samuel-glue-test",
+        bucket_name: "samuel-glue-test",
+        aws_region: "eu-north-1",
+        //@ts-expect-error amplify backend type issue https://github.com/aws-amplify/amplify-backend/issues/2569
+        paths: {
+          "*": {
+            groupsdl: ["get", "list", "write", "delete"],
+          },
+        },
+      },
+      {
         name: "samueeel",
         bucket_name: "samueeel",
         aws_region: "eu-north-1",
@@ -26,17 +37,6 @@ backend.addOutput({
           },
         },
       },
-      {
-        name: "samuel-glue-test",
-        bucket_name: "samuel-glue-test",
-        aws_region: "eu-north-1",
-        //@ts-expect-error amplify backend type issue https://github.com/aws-amplify/amplify-backend/issues/2569
-        paths: {
-          "*": {
-            groupsdl: ["get", "list", "write", "delete"],
-          },
-        },
-      }
     ],
   },
 });
