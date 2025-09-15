a74e93be2e523ebe9d3944d9aa692e1d4b4c5999
SamuelWerneflo <160221396+SamuelWerneflo@users.noreply.github.com>
2025-09-15 13:09:15 +0200

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -7,6 +7,68 @@ import { Policy, PolicyStatement, Effect } from "aws-cdk-lib/aws-iam";
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 const backend = defineBackend({
-  auth,
-  muelBucket
+  auth
 });
+
+backend.addOutput({
+  version: "1.3",
+  storage: {
+    aws_region: "eu-north-1",
+    bucket_name: "samueeel",
+    buckets: [
+      {
+        name: "samueeel",
+        bucket_name: "samueeel",
+        aws_region: "eu-north-1",
+        //@ts-expect-error amplify backend type issue https://github.com/aws-amplify/amplify-backend/issues/2569
+        paths: {
+          "*": {
+            groupsmuel: ["get", "list", "write", "delete"],
+          },
+        },
+      },
+    ],
+  },
+});
+
+
+
+/**
+ * Note: This code assumes the existence of an S3 bucket named 'my-existing-bucket'.
+ * Replace 'my-existing-bucket' with your actual bucket name and adjust the paths and permissions as needed.
+ * For more information on authorization access, visit: https://docs.amplify.aws/react/build-a-backend/storage/authorization/#available-actions
+ *
+ * Requirements for this sample:
+ * 1. An S3 bucket named 'my-existing-bucket' must exist in your AWS account.
+ * 2. The bucket should contain two folders:
+ *    - 'public/' - Accessible by all authenticated and unauthenticated users.
+ *    - 'admin/' - Accessible only by users in the admin group and authenticated users.
+ *
+ * Note: Ensure the bucket exists before deploying this code, as it only sets up IAM policies and does not create the S3 bucket.
+ */
+
+
+/**
+ * Define an inline policy to attach to Admin user role
+ * This policy defines how authenticated users can access your existing bucket
+ */
+const muelPolicy = new Policy(backend.stack, "customBucketMuelPolicy", {
+  statements: [
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+      resources: [`arn:aws:s3:::samueeel/*`],
+    }),
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["s3:ListBucket"],
+      resources: [
+        `arn:aws:s3:::samueeel`,
+        `arn:aws:s3:::samueeel/*`,
+      ],
+    }),
+  ],
+});
+
+// Add the policies to the muel user role
+backend.auth.resources.groups["muel"].role.attachInlinePolicy(muelPolicy);
