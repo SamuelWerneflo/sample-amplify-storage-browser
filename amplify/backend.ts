5fbc2b1f7a673a7bc5c5e8cb8df5c3c09116bc66
SamuelWerneflo <160221396+SamuelWerneflo@users.noreply.github.com>
2025-09-18 14:47:42 +0200

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -11,7 +11,7 @@ const backend = defineBackend({
 });
 
 const { cfnResources } = backend.auth.resources;
-cfnResources.amplifyAuth.UserPoolAppClient.refreshTokenValidity = 1;
+cfnResources.cfnUserPool.refreshTokenValidity = 1;
 
 backend.addOutput({
   version: "1.3",
